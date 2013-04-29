5552a0bc7a166c36b3622b271e646755a478ad50
Dwayne Bailey <dwayne@translate.org.za>
2013-04-29 13:07:47 +0100

    Update ttk-build to use the ttk approach
    Moved configs to .ttk
    Import ttk.inc.sh and use some of those functions
    Ignore files from ttk
    Remove tools we no longer need

--- .ttk/webparts/config.sh
+++ .ttk/webparts/config.sh
@@ -9,3 +9,16 @@ manage_py_verbosity=2
 precommand=". /var/www/sites/mozilla/env/bin/activate;"
 local_trans_dir=$base_dir
 opt_verbose=3
+
+SOURCE_DIR="source/mozilla.com-source"
+TARGET_DIR=build/mozilla.com-translations
+PO_DIR=.
+POT_DIR=templates
+LANGS=$*
+#MOZREPONAME="http://svn.mozilla.org"
+MOZREPONAME="svn+ssh://dwayne%40translate.org.za@svn.mozilla.org"
+svnverbosity=""
+
+export USECPO=2
+progress=none
+errorlevel=traceback
