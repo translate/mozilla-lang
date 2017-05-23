0533a75fb558baf6bc3cfa86a575e32989ac84af
Mozilla Pootle L10n Robot <tech-team@translate.org.za>
2017-05-23 10:41:39 +0000

    Tools: Disable skip languages with all files
    All the languages have been cleaned in the git repository, so there
    is no reason to prevent Pootle from reflecting the git repository status.

--- .ttk/webparts/config.sh
+++ .ttk/webparts/config.sh
@@ -23,7 +23,8 @@ alt_src="bn_IN es es_MX fr ru"
 
 
 # List of languages we skip when removing extra files because they have significantly translated those.
-LANGS_WITH_ALL_FILES="af an as bn_BD br bs ca cak cy en_ZA ff ga_IE hi_IN is lt lv nb_NO ne son ta ur xh"
+#LANGS_WITH_ALL_FILES="af an as bn_BD br bs ca cak cy en_ZA ff ga_IE hi_IN is lt lv ne son ta ur xh"
+LANGS_WITH_ALL_FILES=""
 
 
 # List of files that are always enabled for translation.
